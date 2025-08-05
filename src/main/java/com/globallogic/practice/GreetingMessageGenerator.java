8b35858ce31812e59acf6a64526e71eaf761102e
Dragoș Petrașcu <dragos.petrascu@globallogic.com>
2025-08-05 15:10:26 +0300

    add getMomentOfDay

--- src/main/java/com/globallogic/practice/GreetingMessageGenerator.java
+++ src/main/java/com/globallogic/practice/GreetingMessageGenerator.java
@@ -1,8 +1,25 @@
 package com.globallogic.practice;
 
+import java.util.Calendar;
+
 public class GreetingMessageGenerator {
+
     public String generateGreetingMessage(String fullName) {
-        String momentOfDay = "evenimg";
+        String momentOfDay = getMomentOfDay();
         return "Good " + momentOfDay + ", " + fullName + "!";
+    }
+
+    private String getMomentOfDay() {
+        int hourOfDay = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        if (5 <= hourOfDay && hourOfDay < 12) {
+            return "morning";
+        }
+        if (12 <= hourOfDay && hourOfDay < 18) {
+            return "afternoon";
+        }
+        if (18 <= hourOfDay && hourOfDay < 22) {
+            return "evening";
+        }
+        return "night";
     }
 }
