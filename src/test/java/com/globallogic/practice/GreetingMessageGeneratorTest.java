0e8f2255edbdd4ec85b0b25a5eff547234c3d762
Dragoș Petrașcu <dragos.petrascu@globallogic.com>
2025-08-06 15:19:39 +0300

    use @Mock annotation

--- src/test/java/com/globallogic/practice/GreetingMessageGeneratorTest.java
+++ src/test/java/com/globallogic/practice/GreetingMessageGeneratorTest.java
@@ -3,18 +3,23 @@ package com.globallogic.practice;
 import com.globallogic.practice.clock.Clock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class GreetingMessageGeneratorTest {
 
+    @Mock
     Clock clock;
+
     GreetingMessageGenerator generator;
 
     @BeforeEach
     void setup() {
-        clock = Mockito.mock(Clock.class);
         generator = new GreetingMessageGenerator(clock);
     }
 
